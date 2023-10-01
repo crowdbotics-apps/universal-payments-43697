616c35361716686226aaaa942f46606e56a2ce05
Crowdbotics <team@crowdbotics.com>
2023-10-01 22:09:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "universal-payments-43697",
+  url: "https://universal-payments-43697.botics.co",
+  api: "https://universal-payments-43697.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
